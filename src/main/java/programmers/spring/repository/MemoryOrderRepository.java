9c23244ae504ab2f7f71aa23745f94e177fa2618
wisehero <godjw96@gmail.com>
2022-04-04 16:30:16 +0900

    # 레포지토리 구현체 추가

---
+++ src/main/java/programmers/spring/repository/MemoryOrderRepository.java
@@ -0,0 +1,19 @@
+package programmers.spring.repository;
+
+import org.springframework.stereotype.Repository;
+import programmers.spring.Order;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class MemoryOrderRepository implements OrderRepository {
+
+    private final Map<UUID, Order> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public void insert(Order order) {
+        storage.put(order.getOrderId(), order);
+    }
+}
