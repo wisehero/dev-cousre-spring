0686fd48f02f30a7bd4ffaa7e0e670c01e221fbb
wisehero <godjw96@gmail.com>
2022-04-04 15:16:22 +0900

    # 레포지토리 인터페이스

---
+++ src/main/java/programmers/spring/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package programmers.spring.repository;
+
+import programmers.spring.Order;
+
+public interface OrderRepository {
+
+    void insert(Order order);
+
+}
---
+++ src/main/java/programmers/spring/repository/VoucherRepository.java
@@ -0,0 +1,10 @@
+package programmers.spring.repository;
+
+import programmers.spring.voucher.Voucher;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface VoucherRepository {
+    Optional<Voucher> findById(UUID voucherId);
+}
