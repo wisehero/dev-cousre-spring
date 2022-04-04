a98af39ec0d5054cec923e440c89c743c08576ea
wisehero <godjw96@gmail.com>
2022-04-04 15:41:49 +0900

    # 설정 파일 작성

--- src/main/java/programmers/spring/config/OrderContext.java
+++ src/main/java/programmers/spring/config/OrderContext.java
@@ -1,5 +1,40 @@
 package programmers.spring.config;
 
+import programmers.spring.Order;
+import programmers.spring.repository.OrderRepository;
+import programmers.spring.repository.VoucherRepository;
+import programmers.spring.service.OrderService;
+import programmers.spring.service.VoucherService;
+import programmers.spring.voucher.Voucher;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public class OrderContext {
-    
+
+    public VoucherRepository voucherRepository() {
+        return new VoucherRepository() {
+            @Override
+            public Optional<Voucher> findById(UUID voucherId) {
+                return Optional.empty();
+            }
+        };
+    }
+
+    public OrderRepository orderRepository() {
+        return new OrderRepository() {
+            @Override
+            public void insert(Order order) {
+
+            }
+        };
+    }
+
+    public VoucherService voucherService() {
+        return new VoucherService(voucherRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderService(voucherService(), orderRepository());
+    }
 }
