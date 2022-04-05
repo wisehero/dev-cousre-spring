170aeb063a86bc8bebb7b052be7eaa720fa05c28
wisehero <godjw96@gmail.com>
2022-04-05 16:06:28 +0900

    Update OrderTest.java

--- src/main/java/programmers/spring/OrderTest.java
+++ src/main/java/programmers/spring/OrderTest.java
@@ -1,8 +1,9 @@
 package programmers.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.util.Assert;
-import programmers.spring.config.OrderContext;
-import programmers.spring.voucher.FixedAmountVoucher;
+import programmers.spring.order.OrderItem;
+import programmers.spring.service.OrderService;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -11,9 +12,10 @@ import java.util.UUID;
 public class OrderTest {
     public static void main(String[] args) {
 
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfiguration.class);
+
         var customerId = UUID.randomUUID();
-        var orderContext = new OrderContext();
-        var orderService = orderContext.orderService();
+        var orderService = ac.getBean(OrderService.class);
         var order = orderService.createOrder(customerId, new ArrayList<OrderItem>() {{
             add(new OrderItem(UUID.randomUUID(), 100L, 1));
         }});
