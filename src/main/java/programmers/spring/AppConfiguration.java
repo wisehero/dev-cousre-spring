1ec954647638c3e1e85d817453668fe1d97210cf
wisehero <godjw96@gmail.com>
2022-04-09 02:12:18 +0900

    Update AppConfiguration.java

--- src/main/java/programmers/spring/AppConfiguration.java
+++ src/main/java/programmers/spring/AppConfiguration.java
@@ -1,12 +1,16 @@
 package programmers.spring;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import programmers.spring.config.YamlPropertiesFactory;
+
 
 @Configuration
-@ComponentScan
-@PropertySource("application.properties")
+@ComponentScan(basePackages = "programmers.spring")
+@PropertySource(value = "application.yaml", factory = YamlPropertiesFactory.class)
+@EnableConfigurationProperties
 public class AppConfiguration {
 
 }
